3df5daedf2c1856489ca1c0c5608809590ae77cd
spmictebk <136552954+spmictebk@users.noreply.github.com>
2023-06-14 15:46:27 +0800

    Update cetak.blade.php

--- resources/views/test/cetak.blade.php
+++ resources/views/test/cetak.blade.php
@@ -43,7 +43,7 @@
     <body class="antialiased">
         <div class="container">
             
-            <p align=center><img src="{{ asset('/storage/ebk/avatar/' . Auth::user()->avatar) }}" style="width: 90px; height: 100px; border-radius: 50%"></p>
+            <p align=center><img src="{{ asset('/storage/ebk/avatar/' . Auth::user()->avatar) }}" style="width: 97px; height: 100px; border-radius: 50%"></p>
             
             <table style="height: 90px; width: 100%; border-collapse: collapse; margin-left: auto; margin-right: auto;" border="1">
                 <tbody>
