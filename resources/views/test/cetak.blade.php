8171504c017a4987831c220991679cc49c3aa130
spmictebk <136552954+spmictebk@users.noreply.github.com>
2023-06-15 14:58:19 +0800

    Update cetak.blade.php

--- resources/views/test/cetak.blade.php
+++ resources/views/test/cetak.blade.php
@@ -44,7 +44,7 @@
         <div class="container">
             <br>
             <br>
-            <p align=right><img src="{{ asset('/storage/ebk/avatar/' . Auth::user()->avatar) }}" style="width: 97px; height: 100px; border-radius: 50%"></p>
+            <p align=right><img src="{{ asset('/storage/ebk/avatar/' . $test->avatar_id) }}" style="width: 97px; height: 100px; border-radius: 50%"></p>
             
             <table style="height: 90px; width: 100%; border-collapse: collapse; margin-left: auto; margin-right: auto;" border="1">
                 <tbody>
