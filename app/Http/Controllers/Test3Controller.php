0af786afa5d43940fa821a7bdf2c930bcd18dddb
spmictebk <136552954+spmictebk@users.noreply.github.com>
2023-06-21 14:23:05 +0800

    Update Test3Controller.php

--- app/Http/Controllers/Test3Controller.php
+++ app/Http/Controllers/Test3Controller.php
@@ -531,7 +531,13 @@ class Test3Controller extends Controller
                 ->orderBy('id', 'asc')
                 ->get();
     
-                
+                $tests1 = Test::query()->with('children')->whereNull('ext2_id')
+                ->where('penyemak2' , '!=', Null)
+                ->where('penyemak3' , '==', Null)
+                //->where( 'penyemak1', $user->name )
+                ->where('ic', 'LIKE', "%{$search}%")
+                ->orderBy('id', 'asc')
+                ->get();
                   
                 $tests2 = Test::query()->with('children')->whereNull('ext2_id')
                 ->where('penyemak3' , '!=', Null)
