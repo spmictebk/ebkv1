d570d5a8b9aaebeb496d97d94ad927d28a1ac034
spmictebk <136552954+spmictebk@users.noreply.github.com>
2023-06-21 11:08:54 +0800

    Update listtest3.blade.php

--- resources/views/test1/listtest3.blade.php
+++ resources/views/test1/listtest3.blade.php
@@ -19,55 +19,43 @@
     </div>
 @endif
 
-<!--&nbsp; <a class="btn btn-primary float-right mb-3" href="">Cipta eBK</a>-->
+<!--&nbsp; <a class="btn btn-primary float-right mb-3" href="">Cipta eBK</a>
 
 <button type="button" class="btn btn-success float-right mb-3" data-toggle="modal" data-target=".bd-example-modal-sm">Carian Pegawai</button>
 
-<!-- <a class="btn btn-primary float-right mb-3" href="">filter date</a> -->
-
+<a class="btn btn-primary float-right mb-3" href="">filter date</a> -->
 @forelse ( $tests1 as $test)
 
 <table  class="table table-bordered table-striped table-dark table-sm">
   <thead class="thead-dark">
-    <tr>
-      <th scope="col">No.</th>
-      <th scope="col">Nama</th>
-      <th scope="col">Kad Pengenalan</th>
-      <th scope="col">Sektor</th>
-      <th scope="col">Unit</th>
-      <th scope="col">Jawatan</th>
-      <th scope="col">Gred</th>
-      <th scope="col">Tahun</th>
-      <th scope="col">Perincian</th> 
-      <th scope="col"></th>
-    </tr>
-  </thead>
-  <tbody>
   <tr>
- 
-  <td>{{ ++$i }} </td>
-  <td>{{$test->pyd}}</td>
-  <td>@if (Auth::user()->hasRole('admin'))
-    {{$test->ic}}
-    @else
-    <input type="text" id="copy_{{ $test->id }}" value="{{$test->ic}}" style="color: black; height: 15px; width: 160px;" readonly>
-       <button value="copy" onclick="copyToClipboard('copy_{{ $test->id }}')"><i class="fa fa-copy" style="font-size:14px;color:silver" ></i></button>
-    @endif</td>
-  <td>{{$test->sektor_id}} </td>
-  <td>{{$test->unit_id}} </td>
-  <td>{{$test->jawatan}} </td>
-  <td>{{$test->gred}} </td>
-  <td>{{$test->tahun}} </td>
-  <td>{{$test->created_at}}  </td>
-  <td>
-    <a href="{{ route('cetak.test', $test->id) }}" class="btn btn-info btn-sm fa fa-print" role="button" aria-pressed="true">Papar</a>
-  </td>
+        <th scope="col">Nama</th>
+        <th scope="col">Kad Pengenalan</th>
+        <th scope="col">Tempat Bertugas</th>
+        <th scope="col">Jawatan</th>
+        <th scope="col">Tahun</th>
+        <th scope="col">Perincian</th> 
+        <th scope="col"></th>
+      </tr>
+    </thead>
+    <tbody>
+    <tr>
+
+    <td>{{$test->pyd}}</td>
+    <td>{{$test->ic}}</td>
+    <td>{{$test->unit_id}} {{$test->sektor_id}}</td>
+    <td>{{$test->jawatan}} {{$test->gred}}</td>
+    <td>{{$test->tahun}} </td>
+    <td>{{$test->created_at}}  </td>
+    <td>
+      <a href="{{ route('cetak.test', $test->id) }}" class="btn btn-info btn-sm fa fa-print" role="button" aria-pressed="true">Papar</a>
+    </td>
 
 
   @if (count($test['children']) > 0  )
         
   @include( 'test1.listtest2', ['ext2_id' => $test->children]) 
-
+      
   @endif
 
 @empty  
@@ -88,39 +76,28 @@
 
 <table  class="table table-bordered table-striped table-dark table-sm">
   <thead class="thead-dark">
-    <tr>
-      <th scope="col">No.</th>
-      <th scope="col">Nama</th>
-      <th scope="col">Kad Pengenalan</th>
-      <th scope="col">Sektor</th>
-      <th scope="col">Unit</th>
-      <th scope="col">Jawatan</th>
-      <th scope="col">Gred</th>
-      <th scope="col">Tahun</th>
-      <th scope="col">Perincian</th> 
-      <th scope="col"></th>
-    </tr>
-  </thead>
-  <tbody>
   <tr>
- 
-  <td>{{ ++$i }} </td>
-  <td>{{$test->pyd}}</td>
-  <td>@if (Auth::user()->hasRole('admin'))
-    {{$test->ic}}
-    @else
-    <input type="text" id="copy_{{ $test->id }}" value="{{$test->ic}}" style="color: black; height: 15px; width: 160px;" readonly>
-       <button value="copy" onclick="copyToClipboard('copy_{{ $test->id }}')"><i class="fa fa-copy" style="font-size:14px;color:silver" ></i></button>
-    @endif</td>
-  <td>{{$test->sektor_id}} </td>
-  <td>{{$test->unit_id}} </td>
-  <td>{{$test->jawatan}} </td>
-  <td>{{$test->gred}} </td>
-  <td>{{$test->tahun}} </td>
-  <td>{{$test->created_at}}  </td>
-  <td>
-    <a href="{{ route('cetak.test', $test->id) }}" class="btn btn-info btn-sm fa fa-print" role="button" aria-pressed="true">Papar</a>
-  </td>
+        <th scope="col">Nama</th>
+        <th scope="col">Kad Pengenalan</th>
+        <th scope="col">Tempat Bertugas</th>
+        <th scope="col">Jawatan</th>
+        <th scope="col">Tahun</th>
+        <th scope="col">Perincian</th> 
+        <th scope="col"></th>
+      </tr>
+    </thead>
+    <tbody>
+    <tr>
+
+    <td>{{$test->pyd}}</td>
+    <td>{{$test->ic}}</td>
+    <td>{{$test->unit_id}} {{$test->sektor_id}}</td>
+    <td>{{$test->jawatan}} {{$test->gred}}</td>
+    <td>{{$test->tahun}} </td>
+    <td>{{$test->created_at}}  </td>
+    <td>
+      <a href="{{ route('cetak.test', $test->id) }}" class="btn btn-info btn-sm fa fa-print" role="button" aria-pressed="true">Papar</a>
+    </td>
 
 
   @if (count($test['children']) > 0  )
@@ -129,90 +106,70 @@
       
   @endif
 
-@empty  
-</tr>
+  @empty  
+  </tr>
 
-        <tr>
-            <td></td>
-        </tr>    
-        
-@endforelse
-  </tbody>
+          <tr>
+              <td></td>
+          </tr>    
+          
+  @endforelse
+    </tbody>
 
 
-</table> 
+  </table> 
 
-@forelse ( $tests2 as $test)
+  @forelse ( $tests2 as $test)
 
-<table  class="table table-bordered table-striped table-dark table-sm">
-  <thead class="thead-dark">
+  <table  class="table table-bordered table-striped table-dark table-sm">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">Nama</th>
+        <th scope="col">Kad Pengenalan</th>
+        <th scope="col">Tempat Bertugas</th>
+        <th scope="col">Jawatan</th>
+        <th scope="col">Tahun</th>
+        <th scope="col">Perincian</th> 
+        <th scope="col"></th>
+      </tr>
+    </thead>
+    <tbody>
     <tr>
-      <th scope="col">No.</th>
-      <th scope="col">Nama</th>
-      <th scope="col">Kad Pengenalan</th>
-      <th scope="col">Sektor</th>
-      <th scope="col">Unit</th>
-      <th scope="col">Jawatan</th>
-      <th scope="col">Gred</th>
-      <th scope="col">Tahun</th>
-      <th scope="col">Perincian</th> 
-      <th scope="col"></th>
-    </tr>
-  </thead>
-  <tbody>
-  <tr>
- 
-  <td>{{ ++$i }} </td>
-  <td>{{$test->pyd}}</td>
-  <td>@if (Auth::user()->hasRole('admin'))
-    {{$test->ic}}
-    @else
-    <input type="text" id="copy_{{ $test->id }}" value="{{$test->ic}}" style="color: black; height: 15px; width: 160px;" readonly>
-       <button value="copy" onclick="copyToClipboard('copy_{{ $test->id }}')"><i class="fa fa-copy" style="font-size:14px;color:silver" ></i></button>
-    @endif</td>
-  <td>{{$test->sektor_id}} </td>
-  <td>{{$test->unit_id}} </td>
-  <td>{{$test->jawatan}} </td>
-  <td>{{$test->gred}} </td>
-  <td>{{$test->tahun}} </td>
-  <td>{{$test->created_at}}  </td>
-  <td>
-    <a href="{{ route('cetak.test', $test->id) }}" class="btn btn-info btn-sm fa fa-print" role="button" aria-pressed="true">Papar</a>
-  </td>
+
+    <td>{{$test->pyd}}</td>
+    <td>{{$test->ic}}</td>
+    <td>{{$test->unit_id}} {{$test->sektor_id}}</td>
+    <td>{{$test->jawatan}} {{$test->gred}}</td>
+    <td>{{$test->tahun}} </td>
+    <td>{{$test->created_at}}  </td>
+    <td>
+      <a href="{{ route('cetak.test', $test->id) }}" class="btn btn-info btn-sm fa fa-print" role="button" aria-pressed="true">Papar</a>
+    </td>
 
 
-  @if (count($test['children']) > 0  )
+    @if (count($test['children']) > 0  )
+          
+    @include( 'test1.listtest2', ['ext2_id' => $test->children]) 
         
-  @include( 'test1.listtest2', ['ext2_id' => $test->children]) 
-      
-  @endif
+    @endif
 
-@empty  
-</tr>
+  @empty  
+  </tr>
 
-        <tr>
-            <td></td>
-        </tr>    
-        
-@endforelse
-  </tbody>
+          <tr>
+              <td></td>
+          </tr>    
+          
+  @endforelse
+    </tbody>
 
 
-</table> 
+  </table> 
 
 
-@if ($tests1->total())
-<div class="clearfix">
-    <span style="display: inline-block; vertical-align: middle; line-height: normal;">Papar {{ ($tests1->currentPage() * $tests1->perpage()) - ($tests1->perpage() - 1) }} hingga {{ ($tests1->hasMorePages()) ? ($tests1->currentPage() * $tests1->perpage()) : $tests1->total() }} daripada {{ $tests1->total() }} rekod eKeberhasilan</span>
-    {{ $tests1->links() }}
 </div>
 
-@endif
-
- 
-</div>
-
-<!-- Modal Carian JPN-->
+<!-- Modal Carian JPN
 <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-sm">
     <div class="modal-content">
@@ -222,5 +179,6 @@
     </div>
   </div>
 </div>
+-->
 
 </x-app-layout>
