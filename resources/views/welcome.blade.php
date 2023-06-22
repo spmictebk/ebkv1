6f344d555ad284f965eb34a7f0dc937f55370f9b
spmictebk <136552954+spmictebk@users.noreply.github.com>
2023-06-22 10:21:45 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,6 +17,11 @@ body  {
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- PWA  -->
+        <meta name="theme-color" content="#6777ef"/>
+        <link rel="apple-touch-icon" href="{{ asset('logo.PNG') }}">
+        <link rel="manifest" href="{{ asset('/manifest.json') }}">
 
         <title>Sistem eBK</title>
 
