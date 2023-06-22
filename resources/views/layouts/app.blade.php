46b1459cfb7941b8d0825a194a316f70398db5b9
spmictebk <136552954+spmictebk@users.noreply.github.com>
2023-06-22 10:41:44 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <!-- PWA  -->
+    	<meta name="theme-color" content="#6777ef"/>
+    	<link rel="apple-touch-icon" href="{{ asset('logo.PNG') }}">
+    	<link rel="manifest" href="{{ asset('/manifest.json') }}">
+
         <title>Sistem eBK</title>
 
         <!-- Fonts -->
@@ -121,6 +126,16 @@
                 {{ $slot }}
             </main>
         </div>
+
+        <!-- PWA Script -->
+    	<script src="{{ asset('/sw.js') }}"></script>
+    	<script>
+    	    if (!navigator.serviceWorker.controller) {
+    	        navigator.serviceWorker.register("/sw.js").then(function (reg) {
+    	            console.log("Service worker has been registered for scope: " + reg.scope);
+    	        });
+        }
+    	</script> 
         
     </body>
 
