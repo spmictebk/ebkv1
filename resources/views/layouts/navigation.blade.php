e9dad54fb2c7be1200135eb511155b4c516881d4
spmictebk <136552954+spmictebk@users.noreply.github.com>
2023-06-26 12:58:31 +0800

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -85,7 +85,11 @@
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                             <div>{{ Auth::user()->name }}</div>
-                            <img src="{{ asset('/storage/ebk/avatar/' . Auth::user()->avatar) }}" style="width: 70px; border-radius: 10%">
+                            @if ( Auth::user()->avatar != NULL)
+                            &nbsp;<img src="{{ asset('/storage/ebk/avatar/' . Auth::user()->avatar) }}" style="width: 70px; border-radius: 10%">
+                            @else
+                            &nbsp;<img src="{{ asset('/storage/ebk/avatar/unknown.png') }}" style="width: 70px; border-radius: 10%">
+                            @endif
                             <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
